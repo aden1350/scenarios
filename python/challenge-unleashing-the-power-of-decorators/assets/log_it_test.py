6c3081ec6d6f4104d1a124b7f2ba44a8f63266f5
aden1350 <aden1350@gmail.com>
2023-04-07 23:22:03 +1000

    update log_it_test.py with new test file generated from chatgpt

--- python/challenge-unleashing-the-power-of-decorators/assets/log_it_test.py
+++ python/challenge-unleashing-the-power-of-decorators/assets/log_it_test.py
@@ -10,19 +10,47 @@ class TestLogItDecorator(unittest.TestCase):
         if os.path.exists('log.txt'):
             os.remove('log.txt')
     
-    def test_decorator(self):
+    def test_decorator_should_log_function_call_to_file(self):
         @log_it
         def my_function():
             return "done"
-        
+
         result = my_function()
         self.assertEqual(result, "done")
-        
+
         with open('log.txt') as log_file:
             log_content = log_file.read()
-        
-        expected_log_content = f"Timestamp: <timestamp>, Function: my_function, Arguments: (), {{}}\n"
+
+        expected_log_content = f"Timestamp: {time.time()}, Function: my_function, Arguments: (), {{}}\n"
         self.assertIn(expected_log_content, log_content)
 
-if __name__ == "__main__":
+    def test_decorator_should_log_function_call_with_args_to_file(self):
+        @log_it
+        def my_function(a, b, c):
+            return a + b + c
+
+        result = my_function(1, 2, 3)
+        self.assertEqual(result, 6)
+
+        with open('log.txt') as log_file:
+            log_content = log_file.read()
+
+        expected_log_content = f"Timestamp: {time.time()}, Function: my_function, Arguments: (1, 2, 3), {{}}\n"
+        self.assertIn(expected_log_content, log_content)
+
+    def test_decorator_should_log_function_call_with_kwargs_to_file(self):
+        @log_it
+        def my_function(a, b, c):
+            return a + b + c
+
+        result = my_function(a=1, b=2, c=3)
+        self.assertEqual(result, 6)
+
+        with open('log.txt') as log_file:
+            log_content = log_file.read()
+
+        expected_log_content = f"Timestamp: {time.time()}, Function: my_function, Arguments: (), {{'a': 1, 'b': 2, 'c': 3}}\n"
+        self.assertIn(expected_log_content, log_content)
+
+if __name__ == '__main__':
     unittest.main()
