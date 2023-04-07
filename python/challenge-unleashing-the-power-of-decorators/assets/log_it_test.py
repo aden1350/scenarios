e2ac17a733ef6256e74bdb196117911a1a5be956
aden1350 <aden1350@gmail.com>
2023-04-07 23:43:08 +1000

    update on log_it_test.py by formatting Timestamp differently

--- python/challenge-unleashing-the-power-of-decorators/assets/log_it_test.py
+++ python/challenge-unleashing-the-power-of-decorators/assets/log_it_test.py
@@ -10,21 +10,21 @@ class TestLogItDecorator(unittest.TestCase):
     def setUp(self):
         if os.path.exists('log.txt'):
             os.remove('log.txt')
-    
+
     def test_decorator(self):
         @log_it
         def my_function():
             return "done"
-        
+
         result = my_function()
         self.assertEqual(result, "done")
-        
+
         with open('log.txt') as log_file:
             log_content = log_file.read()
-        
-        expected_log_content = f"Timestamp: {time.time()}, Function: my_function, Arguments: (), {{}}"
+
+        expected_log_content = f"Timestamp: \\d+\\.\\d+, Function: my_function, Arguments: \\(\\), {{}}\n"
         actual_log_content = log_content.replace('\n', '')
-        self.assertRegex(actual_log_content, re.escape(expected_log_content))
+        self.assertRegex(actual_log_content, expected_log_content)
 
 if __name__ == "__main__":
     unittest.main()
