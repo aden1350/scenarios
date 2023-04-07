712f81a84b58e11b0353614bacd2cdebd5022032
aden1350 <aden1350@gmail.com>
2023-04-07 23:34:38 +1000

    another update on log_it_test

--- python/challenge-unleashing-the-power-of-decorators/assets/log_it_test.py
+++ python/challenge-unleashing-the-power-of-decorators/assets/log_it_test.py
@@ -22,7 +22,7 @@ class TestLogItDecorator(unittest.TestCase):
         with open('log.txt') as log_file:
             log_content = log_file.read()
         
-        expected_log_content = f"Timestamp: <timestamp>, Function: my_function, Arguments: (), {{}}\n"
+        expected_log_content = f"Timestamp: {time.time()}, Function: my_function, Arguments: (), {{}}\n"
         actual_log_content = log_content.replace('\n', '')
         self.assertIn(expected_log_content, actual_log_content)
 
